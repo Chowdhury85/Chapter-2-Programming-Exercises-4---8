f3a5b47bf7ce7d686a35944fe1a08089665779b5
Chowdhury85 <145401540+Chowdhury85@users.noreply.github.com>
2025-02-10 20:01:45 -0500

    Update QuartsToGallons.java

--- QuartsToGallons.java
+++ QuartsToGallons.java
@@ -1,9 +1,9 @@
 public class QuartsToGallons {
     public static void main(String[] args) {
-        final int QUARTS_IN_GALLON = 4; // Constant for number of quarts in a gallon
-        int quartsNeeded = 18; // Number of quarts needed for the job
-        int gallons = quartsNeeded / QUARTS_IN_GALLON; // Whole gallons
-        int remainingQuarts = quartsNeeded % QUARTS_IN_GALLON; // Remaining quarts
+        final int QUARTS_IN_GALLON = 4;
+        int quartsNeeded = 18;
+        int gallons = quartsNeeded / QUARTS_IN_GALLON;
+        int remainingQuarts = quartsNeeded % QUARTS_IN_GALLON;
 
         System.out.println("A job that needs " + quartsNeeded + " quarts requires " + gallons + " gallons plus " + remainingQuarts + " quarts.");
     }
